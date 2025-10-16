10ef1dd1d1f9dfe2797ae798ec4030b529a9f3ff
SofienBenKtib <sofienbenktib11@gmail.com>
2025-10-16 16:47:22 +0100

    adding image attributes to the product

--- doteat/Models/Product.cs
+++ doteat/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace doteat.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace doteat.Models
 {
     public class Product
     {
@@ -8,6 +10,9 @@
         public decimal Price { get; set; }
         public int Stock { get; set; }
         public int CategoryId { get; set; }
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
+        public string ImageUrl { get; set; } = "https://placehold.co/600x400/000000/FFF";
         public Category? Category { get; set; } //1-to-1 => A product belongs to a category
         public ICollection<OrderItem>? OrderItems { get; set; } // 1-to-Many a product can be in many order items
         public ICollection<ProductIngredient>? ProductIngredients { get; set; } // 1-to-Many a product can be in many order items 
